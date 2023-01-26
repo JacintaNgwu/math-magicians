10fa268365a864854c2fe3f450d4446d6132536f
JacintaNgwu <jacintangwu9900@gmail.com>
2023-01-26 11:11:21 +0530

    Added Home.js for home page

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  <div className="home">
+    <h1>Welcome to our page!</h1>
+    <p>
+      Math magicians is a website for all fans of mathematics.
+      It allows users to make simple calculations and read a random math-related quotes.
+    </p>
+  </div>;
+};
+export default Home;
