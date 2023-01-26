d9a2b0515c3c63c561ee0552b6643db27fc3bddf
JacintaNgwu <jacintangwu9900@gmail.com>
2023-01-26 11:36:57 +0530

    Updated calculator.js as a page

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -19,44 +19,57 @@ const Calculator = () => {
 
   const { total, next } = state;
   return (
-    <div className="calculator">
-      <div className="calculator__display">
-        <div className="calculator__display__current">
-          { next || total || 0}
+    <>
+      <div className="calculate">
+        <div className="math">
+          <h1>Let&apos;s do some math!</h1>
+        </div>
+
+        <div className="calculator">
+          <div className="calculator__display">
+            <div className="calculator__display__current">
+              { next || total || 0}
+            </div>
+          </div>
+
+          <div className="calculator__buttons">
+            <div className="calculator__buttons__row">
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>AC</button>
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>+/-</button>
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>%</button>
+              <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>&#247;</button>
+            </div>
+
+            <div className="calculator__buttons__row">
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>7</button>
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>8</button>
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>9</button>
+              <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>x</button>
+            </div>
+
+            <div className="calculator__buttons__row">
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>4</button>
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>5</button>
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>6</button>
+              <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>-</button>
+            </div>
+
+            <div className="calculator__buttons__row">
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>1</button>
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>2</button>
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>3</button>
+              <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>+</button>
+            </div>
+
+            <div className="calculator__buttons__row">
+              <button type="button" className="calculator__buttons__row__button  zero" onClick={handleClick}>0</button>
+              <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>.</button>
+              <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>=</button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="calculator__buttons">
-        <div className="calculator__buttons__row">
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>AC</button>
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>+/-</button>
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>%</button>
-          <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>&#247;</button>
-        </div>
-        <div className="calculator__buttons__row">
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>7</button>
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>8</button>
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>9</button>
-          <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>x</button>
-        </div>
-        <div className="calculator__buttons__row">
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>4</button>
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>5</button>
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>6</button>
-          <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>-</button>
-        </div>
-        <div className="calculator__buttons__row">
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>1</button>
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>2</button>
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>3</button>
-          <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>+</button>
-        </div>
-        <div className="calculator__buttons__row">
-          <button type="button" className="calculator__buttons__row__button  zero" onClick={handleClick}>0</button>
-          <button type="button" className="calculator__buttons__row__button" onClick={handleClick}>.</button>
-          <button type="button" className="calculator__buttons__row__button  orange" onClick={handleClick}>=</button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
