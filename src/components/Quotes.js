82c5fff8fec67a681c0a74469c4a9b3cfe2f41ee
JacintaNgwu <jacintangwu9900@gmail.com>
2023-01-26 11:19:02 +0530

    Created quotes.js for quotes page

---
+++ src/components/Quotes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quotes = () => (
+  <div className="quote">
+    <p className="quote-text">
+      &rsquo;Mathematics is not about numbers, equations, computations,
+      or algorithms: it is about understanding.&rsquo; –William Paul Thurston
+    </p>
+  </div>
+);
+
+export default Quotes;
